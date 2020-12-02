a9bee971a7b0e9213584825a26e353bea24c9410
Morteza Rezvani <smrezvani.com@gmail.com>
2020-12-02 14:39:11 -0500

    Add some new config

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # darsplus-bbb-config
+
 Script to config BBB for darsplus
+
+# IMPORTANT
+
+## this script made for personal use, with specific configuration. Never, Ever run this in your BBB server
---
+++ config.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# Pull in the helper functions for configuring BigBlueButton
+source /etc/bigbluebutton/bbb-conf/apply-lib.sh
+
+# Latest version of properties
+cat << EOF
+╔══════════════════════════════════════════════════════════════════════════=╗
+║                                                                           ║
+║ Copy the latest version of "bbigbluebutton.properties" file to respective ║
+║ destination with correct permission                                       ║
+║                                                                           ║
+╚═══════════════════════════════════════════════════════════════════════════╝
+EOF
+cp /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties bigbluebutton.properties.org
+cp bigbluebutton.properties /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
+chmod 444 /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
+sleep 5
+
+cat << EOF
+╔═════════════════════════════════════════════════════=╗
+║                                                      ║
+║       Input server FQDN withouth http or https       ║
+║       something like this: bbb.domain.com            ║
+║                                                      ║
+╚══════════════════════════════════════════════════════╝
+EOF
+
+FQDN=$(sed -n -e '/screenshareRtmpServer/ s/.*\= *//p' bigbluebutton.properties.org)
+SALT=$(sed -n -e '/securitySalt/ s/.*\= *//p' bigbluebutton.properties)
+
+sed -i "s,^bigbluebutton.web.serverURL=.*,bigbluebutton.web.serverURL=https://$FQDN,g" /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
+sed -i "s,^screenshareRtmpServer=.*,screenshareRtmpServer=$FQDN,g" /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
+sed -i "s,^securitySalt=.*,securitySalt=$SALT,g" /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
+
+#       latest version of settings
+echo "  - change seting to html5"
+HTML5_CONFIG=/usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml
+
+yq w -i $HTML5_CONFIG public.app.audioChatNotification true
+yq w -i $HTML5_CONFIG public.app.clientTitle DarsPlus Live Session
+yq w -i $HTML5_CONFIG public.app.appName DarsPlus client
+yq w -i $HTML5_CONFIG public.app.copyright "@2020 DarsPlus ltd."
+yq w -i $HTML5_CONFIG public.app.helpLink https://darsplus.com/liveclass/
+yq w -i $HTML5_CONFIG public.app.enableNetworkInformation true
+#yq w -i $HTML5_CONFIG public.app.enableLimitOfViewersInWebcam true
+yq w -i $HTML5_CONFIG public.app.mirrorOwnWebcam true
+yq w -i $HTML5_CONFIG public.app.breakoutRoomLimit 2
+yq w -i $HTML5_CONFIG public.app.defaultSettings.application.overrideLocale fa
+yq w -i $HTML5_CONFIG public.kurento.wsUrl wss://$FQDN/bbb-webrtc-sfu
+yq w -i $HTML5_CONFIG public.captions.fontFamily Vazir
+yq w -i $HTML5_CONFIG public.note.enabled true
+yq w -i $HTML5_CONFIG public.note.url https://$FQDN/pad
+yq w -i $HTML5_CONFIG public.note.config.showLineNumbers true
+yq w -i $HTML5_CONFIG public.note.config.rtl true
+yq w -i $HTML5_CONFIG public.clientLog.external.enabled true
+
+sleep 1
+chmod 444 /usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml
+chown 995:995 /usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml
+
+cat << EOF
+╔═════════════════════════════════════════════════════=╗
+║                                                      ║
+║       Change HTML5 client & default web page         ║
+║                                                      ║
+╚══════════════════════════════════════════════════════╝
+EOF
+
+sed -i "s,^font-family:.*,font-family:'Vazir',g" /usr/share/meteor/bundle/programs/web.browser/head.html
+
+sed -i '2i<link href="https://cdn.jsdelivr.net/gh/rastikerdar/vazir-font@v26.0.2/dist/font-face.css" rel="stylesheet" type="text/css" />'
+
+cd /var/www/bigbluebutton-default && rm -rf * && wget https://github.com/smrezvani/bbb-default-page/archive/main.zip && unzip main.zip
+sleep 2
+
+cat << EOF
+╔════════════════════════════════════════════=╗
+║                                             ║
+║       All setting done successfully         ║
+║                                             ║
+╚═════════════════════════════════════════════╝
+EOF
+
+enableUFWRules
